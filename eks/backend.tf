172dc833cda86f33b0f08875dee8cdc3529e83f2
Ubuntu <ubuntu@ip-172-30-0-155.ec2.internal>
2026-09-22 06:30:36 +0000

    cloned practice repo

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "divyansh-terraform-state-530700193347"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
